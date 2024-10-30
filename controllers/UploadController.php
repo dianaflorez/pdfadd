97cd3c666af31ea1658dfe2131c34fd55e9adbb5
dianaflorez <dianaflorezbravo@gmail.com>
2024-10-30 13:30:42 -0500

    [fix] exito

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -197,6 +197,7 @@ class UploadController extends Controller
 
 
         foreach (glob($uploadPathConverted . '*.pdf') as $file2) {
+            echo "dentro converte";
             echo $file2."<br>";
             if (is_file($file2)) {
                 unlink($file2); // Elimina cada archivo
